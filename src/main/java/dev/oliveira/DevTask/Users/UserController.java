5f698e131e455a038fd7d6308244200f345b0dd8
pedro oliveira <pvmgo123@gmail.com>
2025-05-23 17:14:02 -0300

    feat: finish user CRUD with H2 DataBase

--- src/main/java/dev/oliveira/DevTask/Users/UserController.java
+++ src/main/java/dev/oliveira/DevTask/Users/UserController.java
@@ -1,9 +1,14 @@
 package dev.oliveira.DevTask.Users;
 
 import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+@Slf4j
 @RestController
 @RequestMapping("/users")
 public class UserController {
@@ -14,6 +19,10 @@ public class UserController {
     // CREATE
     @PostMapping()
     public UserModel createUser(@RequestBody UserModel user) {
+        log.info("POST /users - Body: {}", user);
+
+        user.setStatus(Boolean.FALSE);
+
         return userRepository.save(user);
     }
 
@@ -21,7 +30,45 @@ public class UserController {
     // READ ALL
     @GetMapping()
     public List<UserModel> getAll() {
+        log.info("GET /users");
+
         return userRepository.findAll();
+    }
+
+    // READ BY ID
+    @GetMapping("/{id}")
+    public UserModel getById(@PathVariable Long id) {
+        log.info("GET /users/{}", id);
+
+        return userRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found"));
+    }
+
+    // UPDATE
+    @PutMapping("/{id}")
+    public UserModel updateUser(@PathVariable Long id, @RequestBody UserModel userDetails) {
+        log.info("PUT /users/{} - Body: {}", id, userDetails);
+
+        UserModel user = userRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found"));
+
+        user.setNome(userDetails.getNome());
+        user.setEmail(userDetails.getEmail());
+        user.setIdade(userDetails.getIdade());
+
+        return userRepository.save(user);
+    }
+
+    // DELETE
+    @DeleteMapping("/{id}")
+    public UserModel deleteUser(@PathVariable Long id) {
+        log.info("DELETE /users/{}", id);
+
+        UserModel user = userRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found"));
+
+        userRepository.deleteById(id);
+        return user;
     }
 
 }
